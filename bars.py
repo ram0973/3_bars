65381900c7a05ef6a4ef5e40f68b834c973f7311
ram0973 <ram0973you@gmail.com>
2016-10-04 21:26:07 +0300

    pep8 satisfy

--- bars.py
+++ bars.py
@@ -4,22 +4,24 @@ import win_unicode_console
 import math
 
 
-def load_data(filepath):
+def load_data(filepath) -> list:
     with open(filepath, mode='r', encoding="utf-8") as file:
         return json.load(file)
 
 
-def get_biggest_bar(data):
+def get_biggest_bar(data) -> dict:
     return max(data, key=lambda el: el['Cells']['SeatsCount'])
 
 
-def get_smallest_bar(data) -> list:
+def get_smallest_bar(data) -> dict:
     return min(data, key=lambda el: el['Cells']['SeatsCount'])
 
 
-def get_closest_bar(data, longitude, latitude) -> list:
-    return min(data, key=lambda el: math.sqrt((el['Cells']['geoData']['coordinates'][0] - longitude)**2 +
-                                              (el['Cells']['geoData']['coordinates'][1] - latitude)**2))
+def get_closest_bar(data, longitude, latitude) -> dict:
+    return min(data, key=lambda el: math.sqrt(
+        (el['Cells']['geoData']['coordinates'][0] - longitude)**2 +
+        (el['Cells']['geoData']['coordinates'][1] - latitude)**2)
+    )
 
 
 if __name__ == '__main__':
@@ -30,9 +32,10 @@ if __name__ == '__main__':
 
     win_unicode_console.enable()
 
-    print('Бар с минимальным количеством мест:\n', get_smallest_bar(data), '\n')
-    print('Бар с максимальным количеством мест:\n', get_biggest_bar(data), '\n')
-    print('Самый близкий бар:\n', get_closest_bar(data, longitude, latitude), '\n')
+    print('Бар с мин. кол-вом мест:\n', get_smallest_bar(data), '\n')
+    print('Бар с макс. кол-вом мест:\n', get_biggest_bar(data), '\n')
+    print('Самый близкий бар:\n', get_closest_bar(data, longitude, latitude),
+          '\n')
 
 '''
 Json file item example
