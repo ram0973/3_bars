b77afbfb26566f9a519427a296ce67ea63c98a0d
ram0973 <ram0973@gmail.com>
2016-10-06 20:35:16 +0300

    many fixes

--- bars.py
+++ bars.py
@@ -1,35 +1,52 @@
 # -*- coding: utf-8 -*-
+import argparse
 import json
+from os.path import isfile
 from sys import platform
 import math
+import sys
 
-JSON_FILE = 'Бары.json'
+
+def get_named_argument(arg_name: str) -> str:
+    if len(sys.argv) > 1:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--' + arg_name)
+        return getattr(parser.parse_args(sys.argv[1:]), arg_name)
+    else:
+        print('Введите параметр в формате --%s Значение' % arg_name)
+        exit(1)
 
 
-def load_data(filepath) -> list:
+def load_data(filepath: str):
     with open(filepath, mode='r', encoding="utf-8") as file:
         return json.load(file)
 
 
-def get_biggest_bar(data) -> dict:
+def get_biggest_bar(data):
     return max(data, key=lambda el: el['Cells']['SeatsCount'])
 
 
-def get_smallest_bar(data) -> dict:
+def get_smallest_bar(data):
     return min(data, key=lambda el: el['Cells']['SeatsCount'])
 
 
-def get_closest_bar(data, longitude, latitude) -> dict:
+def get_closest_bar(data, longitude: float, latitude: float):
     return min(data, key=lambda el: math.sqrt(
         (el['Cells']['geoData']['coordinates'][0] - longitude)**2 +
         (el['Cells']['geoData']['coordinates'][1] - latitude)**2))
 
 
 if __name__ == '__main__':
-    latitude = float(input('Введите широту вашего местоположения:'))
-    longitude = float(input('Введите долготу вашего местоположения:'))
 
-    data = load_data(JSON_FILE)
+    file_path = get_named_argument('json')
+    if isfile(file_path):
+        data = load_data(file_path)
+    else:
+        print('Файл не найден')
+        exit(1)
+
+    latitude = float(input('Введите широту вашего местоположения: '))
+    longitude = float(input('Введите долготу вашего местоположения: '))
 
     if platform == 'win32':
         import win_unicode_console
